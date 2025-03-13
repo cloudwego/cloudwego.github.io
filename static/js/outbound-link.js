465ea6204e2f0d53de4f63e997769f88e156993f
luoguangming <luoguangming.ivan@bytedance.com>
2025-03-13 10:59:14 +0800

    chore: fix some js warning

--- static/js/outbound-link.js
+++ static/js/outbound-link.js
@@ -1,12 +1,16 @@
 function outboundLinkInBlank(clipboard) {
   document.querySelectorAll('a').forEach(function (link) {
-    var href = link.getAttribute('href');
-    if (
-      href &&
-      href.startsWith('http') &&
-      (!href.startsWith('https://www.cloudwego.io') || !href.startsWith('https://www.cloudwego.cn'))
-    ) {
-      link.setAttribute('target', '_blank');
+    const href = link.getAttribute('href');
+    if (href && href.startsWith('http')) {
+      try {
+        const url = new URL(href);
+        const host = url.host;
+        if (!host.endsWith('cloudwego.io') && !host.endsWith('cloudwego.cn')) {
+          link.setAttribute('target', '_blank');
+        }
+      } catch (e) {
+        console.error('Invalid URL:', href);
+      }
     }
   });
 }
