143ca8eb7a0a8a2d24984525295fb5bcb49af8a0
rogerogers <rogers@rogerogers.com>
2025-03-13 13:08:55 +0800

    chore: matching by primary domain
    Signed-off-by: rogerogers <rogers@rogerogers.com>

--- static/js/outbound-link.js
+++ static/js/outbound-link.js
@@ -1,15 +1,52 @@
+/**
+ * Extracts the top-level domain from a given hostname.
+ *
+ * @param hostname - The full hostname string from which to extract the top-level domain.
+ * @returns The top-level domain string, or the original hostname if it has two or fewer parts.
+ */
+function getTopLevelDomain(hostname) {
+  if (hostname.startsWith("www.")) {
+    hostname = hostname.substring(4);
+  }
+
+  const parts = hostname.split(".").filter(Boolean);
+  if (parts.length > 2) {
+    return parts.slice(-2).join(".");
+  } else {
+    return hostname;
+  }
+}
+
+/**
+ * Extracts the domain without subdomains from a given URL string.
+ *
+ * @param urlString - The URL string from which to extract the domain without subdomains.
+ * @returns The domain without subdomains if the URL is valid, otherwise null.
+ */
+function getDomainWithoutSubdomains(urlString) {
+  try {
+    const url = new URL(urlString);
+    return getTopLevelDomain(url.hostname);
+  } catch (error) {
+    console.error("Invalid URL:", error);
+    return null;
+  }
+}
+
 function outboundLinkInBlank(clipboard) {
-  document.querySelectorAll('a').forEach(function (link) {
-    const href = link.getAttribute('href');
-    if (href && href.startsWith('http')) {
+  document.querySelectorAll("a").forEach(function (link) {
+    const href = link.getAttribute("href");
+    if (href && href.startsWith("http")) {
       try {
         const url = new URL(href);
         const host = url.host;
-        if (!host.endsWith('cloudwego.io') && !host.endsWith('cloudwego.cn')) {
-          link.setAttribute('target', '_blank');
+        const allowedHosts = ["cloudwego.io", "cloudwego.cn"];
+        const domain = getDomainWithoutSubdomains(host);
+        if (!allowedHosts.includes(domain)) {
+          link.setAttribute("target", "_blank");
         }
       } catch (e) {
-        console.error('Invalid URL:', href);
+        console.error("Invalid URL:", href);
       }
     }
   });
